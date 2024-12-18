9b0f9481fd5ccd3787f325aca47c32c84f25cfdb
Cheeshcake <mohamedazizhadjhassen@gmail.com>
2024-12-18 03:46:31 +0100

    fixing responsive bug

--- components/patient/doctors-list.tsx
+++ components/patient/doctors-list.tsx
@@ -57,7 +57,7 @@ export function DoctorsList({ doctors }: DoctorsListProps) {
             placeholder="Search doctors..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="pl-10  bg-patient-background "
+            className="pl-10 md:max-w-[20rem]  bg-patient-background "
           />
           <Search
             className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
