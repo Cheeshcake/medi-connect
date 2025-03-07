090098524cfc4eb52225dbffd92b912f682579d6
Titou-Dh <titoudh1@gmail.com>
2025-03-08 00:39:39 +0100

    update links

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -32,7 +32,7 @@ export default async function AuthButton() {
               disabled
               className="opacity-75 cursor-none pointer-events-none"
             >
-              <Link href="/admin/sign-in">Sign in</Link>
+              <Link href="/sign-in">Sign in</Link>
             </Button>
             <Button
               asChild
@@ -41,7 +41,7 @@ export default async function AuthButton() {
               disabled
               className="opacity-75 cursor-none pointer-events-none"
             >
-              <Link href="/admin/sign-up">Sign up</Link>
+              <Link href="/sign-up">Sign up</Link>
             </Button>
           </div>
         </div>
